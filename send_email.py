4d2dd2efe453709edc07cbbe496d89bb62846a74
Nate Grebelsky <nate.greb@gmail.com>
2020-12-11 22:40:03 -0500

    adding mail functionality

---
+++ send_email.py
@@ -0,0 +1,15 @@
+import os
+import smtplib
+import ssl
+
+
+print('hello')
+# port = 465  # For SSL
+# password = os.environ.get('PASSWORD')
+# email = os.environ.get('EMAIL')
+# # Create a secure SSL context
+# context = ssl.create_default_context()
+#
+# with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+#     server.login(email, password)
+#     server.sendmail(email, 'nate.greb@gmail.com', 'testing')
